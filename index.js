1e259de94e5152083a247347e28767a85cbe2e95
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-02-03 22:46:22 -0800

    Initial core commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+  setup: function(){
+    console.log('got setup');
+  },
+  server: function(){
+    console.log('got server');
+  },
+  database: function(){
+    console.log('got database');
+  }
+}
