20ee7e91accd892ccfee687dfcb69f67f0e5b0a1
EMCP <emcp@whichdegree.co>
2017-08-10 18:35:34 +0200

    Initial testing added

---
+++ test/mahrio-test.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'test';
+let chai = require('chai');
+chai.should();
+
+let mahrio = require('../lib/index.js');
+
+describe('Mahrio', () => {
+  it('should have a runServer function that returns a promise', () => {
+    return mahrio.runServer( process.env, __dirname, function (server) {
+      server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+          directory: {
+            path: ['../public/']
+          }
+        }
+      });
+    }).then(function(mahrio_server) {
+      mahrio_server.info.host.should.equal('127.0.0.1');
+      mahrio_server.info.port.should.equal(6085);
+    });
+  });
+});
