a3f55783c9bfe15ca44539813ba2882075989a5a
emcp <emcp@whichdegree.co>
2019-08-06 20:16:44 +0200

    Removing redundant test for now

--- test/mahrio-test.js
+++ test/mahrio-test.js
@@ -7,23 +7,7 @@ let mahrio = require('../lib/index.js');
 describe('Mahrio', () => {
   it('should have a runServer function that returns a promise', () => {
     return mahrio.runServer( process.env, __dirname, function (server) {
-      server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-          directory: {
-            path: ['../public/']
-          }
-        }
-      });
-    }).then(function(mahrio_server) {
-      mahrio_server.info.host.should.equal('127.0.0.1');
-      mahrio_server.info.port.should.equal(6085);
-    });
-  });
 
-  it('should have support for Server Side Rendering', () => {
-    return mahrio.runServer( process.env, __dirname, function (server) {
       server.route({
         method: 'GET',
         path: '/{param*}',
